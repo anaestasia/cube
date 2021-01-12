33af85131729053baa7cb7b0fb1c0c39c492217f
anaestasia <anaestasia.mathieu@gmail.com>
2021-01-12 18:49:06 +0100

    homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
-function CoolComponent({ adjective = 'Cool' }) {
-  return <p>Youpi So {adjective} !</p>
-}
+import Compo from './components/menu/Compo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/img/logo/logo_ressources_relationnelles.png" className="App-logo" alt="logo" />
         <p>
-          I'm sorry
-          <CoolComponent adjective="awesome" />
-          <CoolComponent />
+          <Compo />
         </p>
       </header>
     </div>
---
+++ src/components/menu/Compo.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Compo extends Component {
+  render() {
+    return <button type="button">BOUTON</button>;
+  }
+}
+
+export default Compo;
