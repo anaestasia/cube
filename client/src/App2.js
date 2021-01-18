1ab0473c79d1047c363a1c379dee63125b5d4c67
aurelien.bertoux <aurelien.bertoux@malrauxbethune.fr>
2021-01-18 15:08:28 +0100

    ajout page 404

--- client/src/App2.js
+++ client/src/App2.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route , Switch } from "react-router-dom";
 import "./App.css";
 import Accueil from "./pages/Accueil";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
+import NotFound from './pages/404/404';
 
 function App2() {
   return (
     <Router>
-      <Route path="/register" exact render={(props) => <Register />} />
-      <Route path="/login" exact render={(props) => <Login />} />
-      <Route path="/Main" exact render={(props) => <Main />} />
-      <Route path="/" exact render={(props) => <Accueil />} />
+      <Switch>
+        <Route exact path="/register" exact render={(props) => <Register />} />
+        <Route exact path="/login" exact render={(props) => <Login />} />
+        <Route exact path="/Main" exact render={(props) => <Main />} />
+        <Route exact path="/" exact render={(props) => <Accueil />} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 }
