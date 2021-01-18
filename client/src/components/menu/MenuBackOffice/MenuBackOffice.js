fcc5a417fa9fe17d746060c33db0f1d1bcb7b620
anaestasia <anaestasia.mathieu@gmail.com>
2021-01-18 17:50:03 +0100

    replace file

--- client/src/components/menu/MenuBackOffice/MenuBackOffice.js
+++ client/src/components/menu/MenuBackOffice/MenuBackOffice.js
@@ -6,25 +6,25 @@ class MenuBackOffice extends Component {
   
     render() {
         return (
-            <li> Back-Office
+            <li><i class="fas fa-user"></i>Back-Office
                 <ul>
-                    <li><i class="fas fa-users-cog"></i> UTILISATEURS
+                    <li><i class="fas fa-tachometer-alt"></i>UTILISATEURS
                         <ul>
                             <li>Gérer les utilisateurs</li>
                         </ul>
                     </li>
-                    <li><i class="fas fa-folder-open"></i>  RESSOURCES
+                    <li><i class="fas fa-tachometer-alt"></i>RESSOURCES
                         <ul>
                             <li>Gérer les ressources</li>
                             <li>Gérer les catégories</li>
                             <li>Gérer les commentaires</li>
                         </ul>
                     </li>
-                    <li><i class="fas fa-info-circle"></i> DOCUMENTATION
+                    <li><i class="fas fa-tachometer-alt"></i>DOCUMENTATION
                         <ul>
                             <li>Visiteur</li>
                             <li>Citoyen</li>
-                            <li>Modérateur</li>
+                            <li>Modérateurs</li>
                             <li>Administrateur</li>
                         </ul>
                     </li>
