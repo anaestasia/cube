74642fea54c0502a0d31b7825afcdcae03a1c331
Mathys FLORENT <mathys.florent@viacesi.fr>
2021-01-19 11:35:44 +0100

    adding menu's features

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,16 @@ import Accueil from "./pages/accueil/Accueil";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
+import DashBoard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
     <Router>
       <Route path="/register" exact render={(props) => <Register />} />
       <Route path="/login" exact render={(props) => <Login />} />
-      <Route path="/Main" exact render={(props) => <Main />} />
-      <Route path="/Accueil" exact render={(props) => <Accueil />} />
+      <Route path="/main" exact render={(props) => <Main />} />
+      <Route path="/" exact render={(props) => <Accueil />} />
+      <Route path="/dashboard" exact render={(props) => <DashBoard />} />
     </Router>
   );
 }
--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import "./Accueil.css";
-import LoginForm from '../components/form/LoginForm';
+import "./Dashboard.css";
+import LoginForm from '../../components/form/LoginForm';
 import { Link } from 'react-router-dom';
-import Ressource from '../components/ressource/Ressource';
-import MenuFilter from "../components/menufilter/MenuFilter";
+import Ressource from "../../components/ressource/Ressource";
+import MenuFilter from "../../components/menu/MenuFilter/MenuFilter";
 
 // import NormalUser from "../components/NormalUser";
 // import Mod from "../components/Mod";
@@ -14,12 +14,10 @@ export default function DashBoard() {
 
   return (
     <div>
-      {/* mettre icone */}
-      <LoginForm />
       <Ressource />
       <MenuFilter />
-      <Link to="/main">main menu</Link> {/* ton logo pour entrer dans le site */}
-     
+      <Link to="/main">main menu</Link> 
+
     </div>
   );
 }
