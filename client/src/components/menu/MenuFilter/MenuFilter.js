d6cf46665b47698ab255c509d943e7604f40eb2d
anaestasia <anaestasia.mathieu@gmail.com>
2021-01-19 11:55:08 +0100

    Modif select

--- client/src/components/menu/MenuFilter/MenuFilter.js
+++ client/src/components/menu/MenuFilter/MenuFilter.js
@@ -1,35 +1,25 @@
 import React, { Component } from 'react';
 import './MenuFilter.css';
-import ReactWidgets, { Combobox } from 'react-widgets';
-import Select from '../../form/select/Select';
-
 
 class MenuFilter extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            Combobox: ReactWidgets,
-            typeRessource: ['Intelligence Émotionnelle', 'Monde professionnel', 'Communication']
-        };
-    }
 
     render() {
         return (
             <div className = "combobox"> 
 
-            <i class="fas fa-file-alt"></i> Catégorie
-            <br/>
-                <select name="catégorieressource" id="catégorie-ressource">
+                <span><i class="fas fa-file-alt"></i> Catégorie</span>
+
+                <select name="catégorie-ressource" id="catégorie-ressource">
                     <option value="">--Sélectionner la catégorie--</option>
                     <option value="intelligenceemotionelle"> intelligence Emotionelle</option>
                     <option value="mondeprofessionel"> Monde Professionel</option>
                     <option value="communication"> Communicaton </option>
                 </select>
-            <br/><br/>
-            <i class="fas fa-file-alt"></i> Type de ressource
-                <br />
-                <select name="typeressource" id="type-ressource">
+                    
+                
+                <span><i class="fas fa-file-alt"></i> Type de ressource</span>
+
+                <select name="type-ressource" id="type-ressource">
                     <option value="">--Sélectionner le type de ressource--</option>
                     <option value="article"> Article</option>
                     <option value="atelier"> Atelier</option>
--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import Ressource from '../../components/ressource/Ressource';
-import MenuFilter from "../../components/menufilter/MenuFilter";
+import MenuFilter from '../../components/menu/MenuFilter/MenuFilter';
 
 export default function DashBoard() {
 
