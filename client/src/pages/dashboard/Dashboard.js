d04ef4fd1b9dcd50d9467b06b93ce829ba7131b4
aurelien.bertoux <aurelien.bertoux@malrauxbethune.fr>
2021-01-19 18:58:29 +0100

    erreur

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import "./Accueil.css";
-import LoginForm from '../components/form/LoginForm';
-=======
 import React from "react";
->>>>>>> 2c7b7cb618b3e84ec3a16c4baa55077f121d8cdb
 import { Link } from 'react-router-dom';
 import Ressource from '../../components/ressource/Ressource';
 import MenuFilter from '../../components/menu/MenuFilter/MenuFilter';
