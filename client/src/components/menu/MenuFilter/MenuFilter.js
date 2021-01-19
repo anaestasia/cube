e66158c9d998a741e0b37ee6e2e688444cd8e2bf
Mathys FLORENT <mathys.florent@viacesi.fr>
2021-01-19 16:05:53 +0100

    adding checkboxs

--- client/src/components/menu/MenuFilter/MenuFilter.js
+++ client/src/components/menu/MenuFilter/MenuFilter.js
@@ -8,7 +8,7 @@ class MenuFilter extends Component {
             <div className = "combobox"> 
 
                 <span><i class="fas fa-file-alt"></i> Catégorie</span>
-
+                <br/>
                 <select name="catégorie-ressource" id="catégorie-ressource">
                     <option value="">--Sélectionner la catégorie--</option>
                     <option value="intelligenceemotionelle"> intelligence Emotionelle</option>
@@ -16,14 +16,20 @@ class MenuFilter extends Component {
                     <option value="communication"> Communicaton </option>
                 </select>
                     
-                
+                <br/>
                 <span><i class="fas fa-file-alt"></i> Type de ressource</span>
-
+                <br/>
                 <select name="type-ressource" id="type-ressource">
                     <option value="">--Sélectionner le type de ressource--</option>
                     <option value="article"> Article</option>
-                    <option value="atelier"> Atelier</option>
+                    <option value="activité-jeu"> Activité / Jeu</option>
+                    <option value="carte-défi"> Carte défi</option>
+                    <option value="cours-pdf"> Cours au format PDF</option>
+                    <option value="exercice-atelier"> Exercice / Atelier</option>
+                    <option value="fiche-de-lecture"> Fiche de lecture</option>
+                    <option value="jeu-en-ligne"> Jeu en Ligne</option>
                     <option value="vidéo"> Vidéo</option>
+                    
                 </select>
 
             </div>    
--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import Ressource from '../../components/ressource/Ressource';
 import MenuFilter from '../../components/menu/MenuFilter/MenuFilter';
+import {Checkbox} from 'semantic-ui-react'
+import MenuUser from "../../components/menu/MenuUser/MenuUser";
+
 
 export default function DashBoard() {
 
@@ -9,6 +12,14 @@ export default function DashBoard() {
     <div>
       <Ressource />
       <MenuFilter />
+      <ul>
+        <li><i class="fas fa-user"></i> <Checkbox label={{ children: 'Soi' }} /></li>
+        <li><i class="fas fa-heart"></i> <Checkbox label={{ children: 'Conjoints' }} /></li>
+        <li><i class="fas fa-briefcase"></i> <Checkbox label={{ children: 'Professionelles' }} /></li>
+        <li><i class="fas fa-users"></i> <Checkbox label={{ children: 'Famille' }} /></li>
+      </ul>
+      
+
       <Link to="/main">main menu</Link> 
     </div>
   );
