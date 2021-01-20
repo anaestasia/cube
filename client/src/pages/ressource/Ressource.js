bc83fa6dce7d01fc4705ced69fa97f4b1b06ccfc
anaestasia <anaestasia.mathieu@gmail.com>
2021-01-20 15:16:26 +0100

    install bootstrap

--- client/src/pages/ressource/Ressource.js
+++ client/src/pages/ressource/Ressource.js
@@ -3,16 +3,24 @@ import Menu from "../../components/menu/Menu";
 import Ressource from '../../components/ressource/Ressource';
 import Comment from '../../components/ressource/comment/Comment';
 import './Ressource.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function RessourcePage () {
 
   return (
-    <div className="page">
-        <Menu />
-        <div className="content-page">
-            <Ressource />
-            <Comment />
-        </div>
-    </div>
+    <Container fluid>
+        <Row>
+            <Col xl={3} className="col-menu">
+                <Menu />
+            </Col>
+
+            <Col xl={9} className="col-content-page">
+                <Row><Ressource /></Row>
+                <Row><Comment /></Row>
+            </Col>
+        </Row>
+    </Container>
   );
 }
