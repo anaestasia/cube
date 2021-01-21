d32941688209cd583b49e42a6b249d93a40dc91f
Mathys FLORENT <mathys.florent@viacesi.fr>
2021-01-21 18:59:34 +0100

    test

--- client/src/components/ressource/Ressource.js
+++ client/src/components/ressource/Ressource.js
@@ -1,7 +1,6 @@
 import React, { useState , useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import Axios from "axios";
-
 import './Ressource.css';
 
     export default function Ressource(){
