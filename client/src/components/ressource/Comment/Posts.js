7461e9616bcf015a801a48056696fcc33ae6e5c0
unknown <mathys.florent@viacesi.fr>
2021-03-16 14:17:54 +0100

    adding modification for ckeditor

--- client/src/components/ressource/Comment/Posts.js
+++ client/src/components/ressource/Comment/Posts.js
@@ -4,9 +4,14 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import '@ckeditor/ckeditor5-build-classic/build/translations/fr.js';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import '@ckeditor/ckeditor5-build-classic/build/translations/fr.js';
+
+import 'ckeditor5-cube'
+import ClassicEditor from 'ckeditor5-cube'
+import CKEditor from  'ckeditor5-cube'
+
 import Axios from "axios";
 
 const Posts = ({ posts , idUser , role}) => {
